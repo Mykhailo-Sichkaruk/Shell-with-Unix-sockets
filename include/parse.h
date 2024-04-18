3876a65d57f0d243d8c707427a7e98605b542ae9
Mykhailo Sichkaruk <mykhailo.sichkaruk@gmail.com>
2024-04-18 17:40:27 +0200

    Add `/include/` folder for dll.

--- include/parse.h
+++ include/parse.h
@@ -74,16 +74,5 @@ Sequence parse_sequence(char *sequence);
 Line parse_line(char *line);
 Parser parse(char *input);
 
+#include "parse_utils.h"
 #endif // PARSE_H
-#ifndef PARSE_UTILS_H
-#define PARSE_UTILS_H
-
-#include "parse.h"
-
-void print_file(FileRedirection fileRedirection);
-void print_command(Command command);
-void print_component(SequenceComponent redirection);
-void print_sequence(Sequence sequence);
-void print_line(Line line);
-
-#endif // PARSE_UTILS_H
--- include/parse_utils.h
+++ include/parse_utils.h
@@ -9,4 +9,9 @@ void print_component(SequenceComponent redirection);
 void print_sequence(Sequence sequence);
 void print_line(Line line);
 
+void free_command(Command *cmd);
+void free_sequence_component(SequenceComponent *component);
+void free_sequence(Sequence *sequence);
+void free_line(Line *line);
+void free_parser(Parser *parser);
 #endif // PARSE_UTILS_H
